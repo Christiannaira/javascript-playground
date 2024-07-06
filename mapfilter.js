db41bb422c82efbebd0b82c8d1f18294d314907b
Christian <Christianmnaira@gmail.com>
2024-07-06 17:41:54 +0800

    feat: Map function code challenge 1 solved

--- mapfilter.js
+++ mapfilter.js
@@ -2,8 +2,6 @@
 
 // Array Filter: Given an array of string, filter out all of the strings that have a length greater than 5 characters using array filter and return the filtered array.
 
-// Array Map and Ternary Operator: Given an array of numbers, convert each even number to its square and each odd number to its cube using array map and a ternary operator. Return the modified array. 
-
 let fruits = ["apple", "banana", "pear", "grapefruit", "kiwi"];
 
 
@@ -13,4 +11,18 @@ let filteredFruits = fruits.filter((fruit) => {
 
 });
 
-console.log(filteredFruits);
+// console.log(filteredFruits);
+
+// Array Map and Ternary Operator: Given an array of numbers, convert each even number to its square and each odd number to its cube using array map and a ternary operator. Return the modified array. 
+
+let numbers = [1, 2, 3, 4, 5];
+
+function numberMethod(num){
+
+    return num % 2 === 0? num ** 2 : num ** 3;
+
+}
+
+let mappedNumber = numbers.map(numberMethod);
+
+console.log(mappedNumber);
